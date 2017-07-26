f4803a43caf96af6918d18e31c6dbb40393479e4
Mikołaj Toczek <m.toczek@hotmail.com>
2017-07-26 21:03:26 +0200

    Changed project template. Added calculator subpage and calculator functionality. Added registration & login functionality + authorization.

---
+++ CSharpWebApp2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSharpWebApp2.Startup))]
+namespace CSharpWebApp2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
